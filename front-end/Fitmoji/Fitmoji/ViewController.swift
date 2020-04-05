f1f3d576541a8546eb107ddaa6b492b7e4476a01
wc1404 <wc1404@nyu.edu>
2020-04-05 11:05:24 -0400

    Adding .gitignore File

---
+++ front-end/Fitmoji/Fitmoji/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fitmoji
+//
+//  Created by John Martinez on 3/25/20.
+//  Copyright © 2020 John Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
